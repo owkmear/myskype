41aab25cfd1f1ba72e0ffd85fdb87cefb902013b
michael <askmeaboutmyschoollife@gmail.com>
2016-10-14 09:04:46 +0300

    push on heroku

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var http = require('http');
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
 	file.serve(req, res);
-}).listen(1234);
+});
+port = Number(process.env.PORT || 1234);
+app.listen(1234);
 
 var io = require('socket.io').listen(app);
 
